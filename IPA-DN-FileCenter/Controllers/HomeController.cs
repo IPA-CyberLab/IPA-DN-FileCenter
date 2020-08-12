3b957d633447e12f2c0d5dc990aaec6c10f9000d
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2020-08-12 21:13:48 +0900

    途中さん

--- IPA-DN-FileCenter/Controllers/HomeController.cs
+++ IPA-DN-FileCenter/Controllers/HomeController.cs
@@ -6,16 +6,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using IPA.DN.FileCenter.Models;
+using IPA.Cores.Helper.Basic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using System.Threading;
 
 namespace IPA.DN.FileCenter.Controllers
 {
+    [AutoValidateAntiforgeryToken]
     public class HomeController : Controller
     {
+        readonly Server server;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, Server server)
         {
             _logger = logger;
+
+            this.server = server;
         }
 
         public IActionResult Index()
@@ -26,6 +35,55 @@ namespace IPA.DN.FileCenter.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<IActionResult> UploadAsync(UploadFormRequest form,
+            List<IFormFile> file_1,
+            List<IFormFile> file_2,
+            List<IFormFile> file_3,
+            List<IFormFile> file_4,
+            List<IFormFile> file_5,
+            List<IFormFile> file_6,
+            List<IFormFile> file_7,
+            List<IFormFile> file_8,
+            List<IFormFile> file_9,
+            List<IFormFile> file_10,
+            CancellationToken cancel)
+        {
+            using UploadFileList fl = new UploadFileList();
+
+            fl.AddFormFileList(file_1, form.dirname_1);
+            fl.AddFormFileList(file_2, form.dirname_2);
+            fl.AddFormFileList(file_3, form.dirname_3);
+            fl.AddFormFileList(file_4, form.dirname_4);
+            fl.AddFormFileList(file_5, form.dirname_5);
+            fl.AddFormFileList(file_6, form.dirname_6);
+            fl.AddFormFileList(file_7, form.dirname_7);
+            fl.AddFormFileList(file_8, form.dirname_8);
+            fl.AddFormFileList(file_9, form.dirname_9);
+            fl.AddFormFileList(file_10, form.dirname_10);
+
+            UploadOption opt = new UploadOption
+            {
+                Auth = form.Auth,
+                Days = form.Days,
+                Destination = form.Destination,
+                LogAccess = form.Log,
+                Once = form.Once,
+                UrlHint = form.UrlHint,
+                Zip = form.Zip,
+            };
+
+            opt.Normalize();
+
+            await server.UploadAsync(DateTimeOffset.Now,
+                Request.HttpContext.Connection.RemoteIpAddress._UnmapIPv4().ToString(),
+                Request.GetDisplayUrl(),
+                fl,
+                opt,
+                cancel);
+
+            return View("Privacy");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- IPA-DN-FileCenter/FileCenter.cs
+++ IPA-DN-FileCenter/FileCenter.cs
@@ -68,11 +68,13 @@ namespace IPA.DN.FileCenter
         public const string SingleInstanceKey = "IPA.DN.FileCenter";
         public const string HiveSettingsName = "FileCenter/Settings";
 
-        public const string FileBrowserHttpDir = "/files";
+        public const string FileBrowserHttpDir = "/d";
 
         public const string DefaultWebSiteTitle = "FileCenter";
 
         public const int MaxUploadFiles = 10;
+
+        public const int MaxUrlHintLen = 32;
     }
 
     public class DbHive : INormalizable
@@ -80,12 +82,174 @@ namespace IPA.DN.FileCenter
         public string DataStoreRootDir = "";
         public string PIN = "";
         public string WebSiteTitle = "";
+        public DateTimeOffset LastUploadDateTime;
+        public int LastSeqNo;
 
         public void Normalize()
         {
             if (DataStoreRootDir._IsEmpty()) DataStoreRootDir = FileCenterConsts.DefaultStoreRoot;
 
             if (WebSiteTitle._IsEmpty()) WebSiteTitle = FileCenterConsts.DefaultWebSiteTitle;
+
+            if (LastUploadDateTime._IsZeroDateTime()) LastUploadDateTime = Util.ZeroDateTimeOffsetValue;
+        }
+    }
+
+    public class UploadResult
+    {
+        public string GeneratedUrlDir { get; set; } = "";
+        public string GeneratedUrlDirDirect { get; set; } = "";
+        public string? GeneratedUserName { get; set; }
+        public string? GeneratedPassword { get; set; }
+        public bool IsZipped { get; set; }
+        public string? GeneratedZipPassword { get; set; }
+        public string Destination { get; set; } = "";
+        public DateTimeOffset Expires { get; set; } = Util.MaxDateTimeOffsetValue;
+        public int NumFiles { get; set; }
+        public long TotalFileSize { get; set; }
+    }
+
+    public class UploadFormRequest
+    {
+        public string? Destination { get; set; }
+        public string? UrlHint { get; set; }
+        public bool Auth { get; set; }
+        public bool Log { get; set; }
+        public bool Zip { get; set; }
+        public bool Once { get; set; }
+        public int Days { get; set; }
+
+        public string? dirname_1 { get; set; }
+        public string? dirname_2 { get; set; }
+        public string? dirname_3 { get; set; }
+        public string? dirname_4 { get; set; }
+        public string? dirname_5 { get; set; }
+        public string? dirname_6 { get; set; }
+        public string? dirname_7 { get; set; }
+        public string? dirname_8 { get; set; }
+        public string? dirname_9 { get; set; }
+        public string? dirname_10 { get; set; }
+    }
+
+    public class UploadFile : AsyncService
+    {
+        public Stream Stream { get; }
+        public string RelativeFileName { get; }
+
+        public UploadFile(Stream fileStream, string fileName, string? subDirName = null)
+        {
+            try
+            {
+                fileName = PPWin.GetFileName(fileName);
+
+                if (fileName._IsNullOrZeroLen()) throw new ArgumentNullException(nameof(fileName));
+
+                subDirName = subDirName._NonNullTrim();
+
+                subDirName = subDirName._MakeSafeFileName();
+
+                this.RelativeFileName = (subDirName._IsEmpty() ? "" : subDirName + "/") + fileName;
+
+                this.Stream = fileStream;
+            }
+            catch
+            {
+                this._DisposeSafe();
+                throw;
+            }
+        }
+
+        protected override async Task CleanupImplAsync(Exception? ex)
+        {
+            try
+            {
+                await this.Stream._DisposeSafeAsync();
+            }
+            finally
+            {
+                await base.CleanupImplAsync(ex);
+            }
+        }
+    }
+
+    public class UploadFileList : AsyncService
+    {
+        readonly List<UploadFile> FileListInternal = new List<UploadFile>();
+
+        public IReadOnlyList<UploadFile> FileList => FileListInternal;
+
+        public void AddFormFileList(IEnumerable<IFormFile?>? files, string? subDirName)
+        {
+            if (files != null)
+            {
+                foreach (var file in files)
+                {
+                    if (file != null)
+                    {
+                        UploadFile f = new UploadFile(file.OpenReadStream(), file.FileName, subDirName);
+
+                        AddFileInternal(f);
+                    }
+                }
+            }
+        }
+
+        public void AddFile(Stream fileStream, string fileName, string? subDirName = null)
+        {
+            UploadFile f = new UploadFile(fileStream, fileName, subDirName);
+
+            AddFileInternal(f);
+        }
+
+        void AddFileInternal(UploadFile f)
+        {
+            this.FileListInternal.Add(f);
+        }
+
+        protected override async Task CleanupImplAsync(Exception? ex)
+        {
+            try
+            {
+            }
+            finally
+            {
+                await base.CleanupImplAsync(ex);
+            }
+        }
+    }
+
+    public class UploadOption : INormalizable
+    {
+        public string? Destination { get; set; }
+        public string? UrlHint { get; set; }
+        public bool Auth { get; set; }
+        public bool LogAccess { get; set; }
+        public bool Zip { get; set; }
+        public bool Once { get; set; }
+        public int Days { get; set; }
+
+        public string IpAddress { get; set; } = "";
+
+        public void Normalize()
+        {
+            this.Destination = this.Destination._NonNullTrimSe()._TruncStr(Consts.MaxLens.NormalStringTruncateLen);
+            this.UrlHint = this.UrlHint._Normalize(true, true, false, false);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in UrlHint)
+            {
+                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || (c == '-' || c == '_'))
+                {
+                    sb.Append(c);
+                }
+            }
+
+            this.UrlHint = sb.ToString()._TruncStr(FileCenterConsts.MaxUrlHintLen);
+
+            if (this.Days < 0) this.Days = 0;
+            if (this.Days > 2555000) this.Days = 2555000;
+
+            this.IpAddress = this.IpAddress._NonNull();
         }
     }
 
@@ -117,7 +281,7 @@ namespace IPA.DN.FileCenter
                     getDefaultDataFunc: () => new DbHive(),
                     policy: HiveSyncPolicy.AutoReadWriteFile,
                     serializer: HiveSerializerSelection.RichJson);
-                
+
                 // 設定データベースに記載されているディレクトリを作成
                 CreateRootDirectory();
 
@@ -152,6 +316,149 @@ namespace IPA.DN.FileCenter
         public async Task<HttpResult> ProcessFileBrowserRequestAsync(IPAddress clientIpAddress, int clientPort, string requestPathAndQueryString, HttpRequest request, HttpResponse response, CancellationToken cancel = default)
         {
             return await this.Browser.ProcessRequestAsync(clientIpAddress, clientPort, requestPathAndQueryString, request, response, cancel);
+        }
+
+        readonly AsyncLock DirectoryLock = new AsyncLock();
+
+        // アップロードメイン処理
+        public async Task<ResultOrExeption<UploadResult>> UploadAsync(DateTimeOffset timeStamp, string ipAddress, string baseUrl, UploadFileList fileList, UploadOption option, CancellationToken cancel = default)
+        {
+            option.Normalize();
+
+            string newDirName;
+            string newDirFullPath;
+
+            using (await DirectoryLock.LockWithAwait(cancel))
+            {
+                int seqNo;
+
+                lock (this.DbLock)
+                {
+                    if (this.Db.LastUploadDateTime.LocalDateTime.Date != timeStamp.LocalDateTime.Date)
+                    {
+                        // 日が変わった
+                        this.Db.LastSeqNo = 0;
+                    }
+                    this.Db.LastUploadDateTime = timeStamp;
+
+                    // 連番をインクリメントいたします
+                    seqNo = ++this.Db.LastSeqNo;
+                }
+
+                for (int i = 0; ; i++)
+                {
+                    string yymmddAndSeqNo = timeStamp.LocalDateTime.ToString("yyMMdd") + "_" + seqNo.ToString("D3");
+
+                    int sizeOfRandStr = 16;
+
+                    // ユニークなルートディレクトリ名を決定する
+                    if (option.UrlHint._IsFilled())
+                    {
+                        sizeOfRandStr = 16 - option.UrlHint.Length - 1;
+                    }
+
+                    sizeOfRandStr = Math.Max(sizeOfRandStr, 8);
+
+                    string candidate = yymmddAndSeqNo + "_";
+                    if (option.UrlHint._IsFilled())
+                    {
+                        candidate += option.UrlHint + "_";
+                    }
+
+                    candidate += Str.GenRandPassword(sizeOfRandStr, false).ToLower();
+
+                    string fullPath = PP.Combine(this.RootDirectoryFullPath, candidate);
+
+                    if (await Lfs.IsDirectoryExistsAsync(fullPath, cancel) ||
+                        await Lfs.IsFileExistsAsync(fullPath, cancel))
+                    {
+                        // すでに存在する
+                    }
+                    else
+                    {
+                        // 決定 !!
+                        newDirName = candidate;
+                        newDirFullPath = fullPath;
+                        break;
+                    }
+                }
+            }
+
+            newDirFullPath._Debug();
+
+            // パスワード等を生成
+            UploadResult result = new UploadResult
+            {
+                Destination = option.Destination._NonNull(),
+            };
+
+            if (option.Days <= 0)
+            {
+                result.Expires = Util.MaxDateTimeOffsetValue;
+            }
+            else
+            {
+                result.Expires = timeStamp.LocalDateTime.AddDays(option.Days).AddDays(1).AddSeconds(-1)._AsDateTimeOffset(true);
+            }
+
+            if (option.Auth)
+            {
+                result.GeneratedUserName = Str.GenRandPassword(8, false);
+                result.GeneratedPassword = Str.GenRandPassword(24, true);
+            }
+
+            if (option.Zip)
+            {
+                result.GeneratedZipPassword = Str.GenRandPassword(32, true);
+                result.IsZipped = true;
+            }
+
+            // ディレクトリ作成
+            await Lfs.CreateDirectoryAsync(newDirFullPath, flags: FileFlags.OnCreateSetCompressionFlag, cancel: cancel);
+
+            if (false)
+            {
+                // 暗号化 ZIP なしの場合、アップロードされてきたファイルを生ファイルシステムにそのまま書いていく
+                foreach (var file in fileList.FileList)
+                {
+                    string newFileFullPath = Lfs.PP.Combine(newDirFullPath, file.RelativeFileName);
+                    if (option.Auth)
+                    {
+                        newFileFullPath += Consts.Extensions.EncryptedXtsAes256;
+                    }
+
+                    using var newFileObj = await Lfs.CreateAsync(newFileFullPath, flags: FileFlags.AutoCreateDirectory, doNotOverwrite: true, cancel: cancel);
+                    using Stream newFileStream = option.Auth == false ? (Stream)newFileObj.GetStream(true) : new XtsAesRandomAccess(newFileObj, result.GeneratedPassword!, true).GetStream(true);
+                    await file.Stream.CopyBetweenStreamAsync(newFileStream, cancel: cancel, flush: true);
+                }
+            }
+            else
+            {
+                // 暗号化 ZIP ありの場合、すべてのファイルを ZIP で圧縮・暗号化した上で 1 つの ZIP ファイルとして保存する
+                string zipFileFullPath = Lfs.PP.Combine(newDirFullPath, "test.zip");
+                if (option.Auth)
+                {
+                    zipFileFullPath += Consts.Extensions.EncryptedXtsAes256;
+                }
+
+                using var zipOutputFile = await Lfs.CreateAsync(zipFileFullPath, flags: FileFlags.AutoCreateDirectory, doNotOverwrite: true, cancel: cancel);
+                using IRandomAccess<byte> zipOutputFileRandomAccess = option.Auth == false ? (IRandomAccess<byte>)zipOutputFile.GetStream(true) : new XtsAesRandomAccess(zipOutputFile, result.GeneratedPassword!, true);
+                using var zipWriter = new ZipWriter(new ZipContainerOptions(zipOutputFileRandomAccess));
+
+                foreach (var file in fileList.FileList)
+                {
+                    var metadata = new FileMetadata(attributes: FileAttributes.Normal, creationTime: timeStamp, lastWriteTime: timeStamp, lastAccessTime: timeStamp);
+
+                    await zipWriter.ImportVirtualFileAsync(file.Stream,
+                        new FileContainerEntityParam(file.RelativeFileName, metadata,
+                            FileContainerEntityFlags.EnableCompression | FileContainerEntityFlags.CompressionMode_Fast,
+                        encryptPassword: "a"), cancel);
+                }
+
+                await zipWriter.FinishAsync(cancel);
+            }
+
+            return new ResultOrExeption<UploadResult>(result);
         }
 
         // 設定データベースに記載されているディレクトリを作成
